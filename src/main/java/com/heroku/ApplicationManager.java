ee743cb04814245c0d065092e64b9c5058a61839
rt-heroku <rtorres@heroku.com>
2017-02-02 13:38:46 -0500

    more

--- src/main/java/com/heroku/ApplicationManager.java
+++ src/main/java/com/heroku/ApplicationManager.java
@@ -2,23 +2,27 @@ package com.heroku;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.heroku.security.entities.UserAccount;
 import com.heroku.security.repositories.UserRepository;
 import com.heroku.security.services.UserService;
 
 @Configuration
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = WebMvcAutoConfiguration.class)
 public class ApplicationManager {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationManager.class, args);
+        //SpringApplication.run(ApplicationManager.class, args);
+    	ConfigurableApplicationContext ctx = new SpringApplicationBuilder(ApplicationManager.class)
+                .web(false)
+                .run(args);
     }
     
     @Autowired
